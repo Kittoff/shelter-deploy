c521c8964b7b1894fe5e112620410a2521794621
LOZANO <christophe.lozano@capgemini.com>
2020-08-06 14:45:23 +0200

    favicon + name

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import './css/App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // Components 
-import Nav from './components/header/Nav'
-// import Header from './components/header/Header'
-// import About from './components/about/About'
-// import Pets from './components/pets/Pets'
-// import Help from './components/help/Help'
-// import Donation from './components/donation/Donation'
 import Footer from './components/footer/Footer'
 import OurPets from './components/ourPets/OurPets';
 import Global from './components/global/Global';

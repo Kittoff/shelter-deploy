319189e43b375d07d0942f994225e5c66c01a78b
LOZANO <christophe.lozano@capgemini.com>
2020-08-07 15:26:01 +0200

    responsive first commit

--- src/components/ourPets/Pagination.js
+++ src/components/ourPets/Pagination.js
@@ -2,6 +2,23 @@ import React from 'react'
 import { Pagination as Pagi } from 'semantic-ui-react'
 const Pagination = ({ petsPerPage, totalPets, paginate, currentPage }) => {
     const pageNumbers = []
+    // const styles = {
+    //     test: {
+    //         display: "inline-block",
+    //         border: "2px solid #f1d0b8",
+    //         borderRadius: "100px",
+    //         height: "60px",
+    //         width: "60px",
+    //         lineHeight: "60px",
+    //         // textAlign: "center",
+    //         fontSize: "20px",
+    //         cursor: "pointer",
+    //         fontFamily: "'Times New Roman', Times, serif",
+    //         marginTop: window.innerWidth <= 765 ? "400px" : "0",
+    //         marginRight: "18px",
+    //         background: window.innerWidth <= 765 ? "red" : "black",
+    //     }
+    // }
     for (let i = 1; i <= Math.ceil(totalPets / petsPerPage); i++) {
         pageNumbers.push(i);
     }

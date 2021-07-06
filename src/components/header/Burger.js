9b34827dc63d5426d5a162cd0757f54c9b338817
chlozano <christophe.lozano@capgemini.com>
2021-07-06 17:04:39 +0200

    test

--- src/components/header/Burger.js
+++ src/components/header/Burger.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import Rightbar from './Rightbar'
+import React, { useState } from "react";
+import styled from "styled-components";
+import Rightbar from "./Rightbar";
 
 const StyledBurger = styled.div`
   width: 2rem;
@@ -18,24 +18,24 @@ const StyledBurger = styled.div`
   div {
     width: 2rem;
     height: 0.25rem;
-    background-color: ${({ open }) => open ? '#ccc' : '#fddcc4'};
+    background-color: ${({ open }) => (open ? "#ccc" : "#fddcc4")};
     border-radius: 10px;
     transform-origin: 1px;
     transition: all 0.3s linear;
     &:nth-child(1) {
-      transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
+      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
     }
     &:nth-child(2) {
-      transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0)'};
-      opacity: ${({ open }) => open ? 0 : 1};
+      transform: ${({ open }) => (open ? "translateX(100%)" : "translateX(0)")};
+      opacity: ${({ open }) => (open ? 0 : 1)};
     }
     &:nth-child(3) {
-      transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
+      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
     }
   }
 `;
 const Burger = () => {
-  const [open, setopen] = useState(false)
+  const [open, setopen] = useState(false);
   return (
     <>
       <StyledBurger open={open} onClick={() => setopen(!open)}>
@@ -45,7 +45,7 @@ const Burger = () => {
       </StyledBurger>
       <Rightbar open={open} />
     </>
-  )
-}
+  );
+};
 
-export default Burger
+export default Burger;
--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Link as Scroll } from 'react-scroll';
-import { Link } from 'react-router-dom'
-import { HashLink } from 'react-router-hash-link'
-import Rightbar from './Rightbar';
-import Burger from './Burger';
+import React from "react";
+import { Link as Scroll } from "react-scroll";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import Rightbar from "./Rightbar";
+import Burger from "./Burger";
+
 function Nav() {
-    return (
-        <>
-            <div className="header-title">
-                <a href="#">
-                    <h3>Cozy House</h3>
-                    <span className="header-subtitle">Shelter for pets in Boston</span>
-                </a>
-            </div>
+  return (
+    <>
+      <div className="header-title">
+        <a href="#">
+          <h3>Cozy House</h3>
+          <span className="header-subtitle">Shelter for pets in Boston</span>
+        </a>
+      </div>
 
-            <div className="cb"></div>
-            <Burger />
-
-        </>
-    );
+      <div className="cb"></div>
+      <Burger />
+    </>
+  );
 }
 
 export default Nav;
